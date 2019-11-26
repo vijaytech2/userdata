9239acbeadba1b59e5bb5011ef36226906795aa6
vijaytech2 <vijayitchn@gmail.com>
2019-11-26 17:59:37 +0530

    added some values

--- installs_httpd_centos.sh
+++ installs_httpd_centos.sh
@@ -6,3 +6,5 @@ systemctl enable httpd
 cd /var/www/html
 echo "<html><h1> My web Server </h1></html>" > index.html
 curl http://169.254.169.254/latest/meta-data/local-ipv4 >> index.html
+EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+echo "<h1>Hello Vijay from $(hostname -f) in AZ $EC2_AVAIL_ZONE</h1>" > /var/www/html/index.html
--- installs_httpd_ubuntu.sh
+++ installs_httpd_ubuntu.sh
@@ -6,3 +6,5 @@ chkconfig httpd on
 cd /var/www/html
 echo "<html><h1> My web Server </h1></html>" > index.html
 curl http://169.254.169.254/latest/meta-data/local-ipv4 >> index.html
+EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+echo "<h1>Hello Vijay from $(hostname -f) in AZ $EC2_AVAIL_ZONE</h1>" > /var/www/html/index.html
